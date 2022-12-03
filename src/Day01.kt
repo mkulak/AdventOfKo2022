8705f2dbea4d6475a56d822872d6dcc30c7bf012
mykhailo.kulak <misha.kulak@traderepublic.com>
2022-12-03 21:31:24 +0100

    add day1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,13 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.fold(0 to 0) { (maximum, current), str ->
+            if (str.isEmpty()) Math.max(maximum, current) to 0 else maximum to current + str.toInt()
+        }.first
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>): Int =
+        input.fold(listOf<Int>() to 0) { (list, acc), str ->
+            if (str.isEmpty()) list + acc to 0 else list to acc + str.toInt()
+        }.first.sortedBy { -it }.take(3).sum()
 
     val input = readInput("Day01")
     println(part1(input))
